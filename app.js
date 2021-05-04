d449e4d8440a4a17ce35ad7df537a3e9660016e5
Sean Joyce <joycesean702@gmail.com>
2021-05-04 21:01:17 +0100

    add start script

--- app.js
+++ app.js
@@ -167,6 +167,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(8000, () => {
-    console.log('Serving on port 8000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
